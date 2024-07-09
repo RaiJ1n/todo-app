2e0ba959f3b46fa8eff8849ef1e59e2ff74390af
RaiJ1n <129413551+RaiJ1n@users.noreply.github.com>
2024-07-09 12:43:26 +0800

    fix composable

--- src/store/TodoStore.js
+++ src/store/TodoStore.js
@@ -1,42 +1,44 @@
-import { ref } from 'vue';
+import { reactive, toRefs } from "vue";
 
-const taskList = ref([]);
 
-const addTask = (taskName) => {
-    if (taskName.trim() !== '') {
-        const newTask = {
-            id: Date.now(),
-            name: taskName.trim(),
-            isCompleted: false,
-        };
-        taskList.value.push(newTask);
-    }
-};
+const state = reactive({
+  taskList:[]
+});
 
-const updateTask = (taskId, updatedName) => {
+export default function useTodo() {
+ 
+    const addTask = (taskName) => {
+        if (taskName.trim() !== '') {
+            const newTask = {
+                id: Date.now(),
+                name: taskName.trim(),
+                isCompleted: false,
+            };
+            taskList.value.push(newTask);
+        }
+    };
+   const updateTask = (taskId, updatedName) => {
     if (updatedName.trim() !== '') {
-        taskList.value = taskList.value.map(task =>
+        taskList.value = taskList.value.map(task => 
             task.id === taskId ? { ...task, name: updatedName.trim() } : task
-        );
-    }
-};
+            );
+            }
+            };
+    const removeTask = (taskId) => {
+        taskList.value = taskList.value.filter(task => task.id !== taskId);
+            };
+    const toggleTaskCompletion = (taskId) => {
+        taskList.value = taskList.value.map(task => task.id === taskId ? { ...task, isCompleted: !task.isCompleted } : task);
+            };
+    const getTaskList = () => taskList;
+                        
 
-const removeTask = (taskId) => {
-    taskList.value = taskList.value.filter(task => task.id !== taskId);
-};
-
-const toggleTaskCompletion = (taskId) => {
-    taskList.value = taskList.value.map(task =>
-        task.id === taskId ? { ...task, isCompleted: !task.isCompleted } : task
-    );
-};
-
-const getTaskList = () => taskList;
-
-export default {
-    addTask,
-    updateTask,
-    removeTask,
-    toggleTaskCompletion,
-    getTaskList,
-};
+    return {
+        ...toRefs(state),
+        addTask,
+        updateTask,
+        removeTask,
+        toggleTaskCompletion,
+        getTaskList
+        };
+}
