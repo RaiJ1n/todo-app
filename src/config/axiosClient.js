ce171e75860cc76372e0811904081b15563e469d
RaiJ1n <129413551+RaiJ1n@users.noreply.github.com>
2024-07-23 09:36:32 +0800

    new

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:5000/todo',
-  withCredentials: true,
-  
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default apiClient;
--- src/store/TodoStore.js
+++ src/store/TodoStore.js
@@ -6,59 +6,17 @@ const taskList = ref([]);
 export function useTodo() {
   const fetchTodo = async () => {
     try {
-      const response = await apiClient.get("/all/task");
-      taskList.value = response.data.content;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const addTask = async (taskName) => {
-    try {
+      const newUser = await apiClient.get("/");
+      taskList.value = newUser.data.content;
       
-      await apiClient.post("/add/task", { name: todo });
-      await fetchTodo(); 
     } catch (err) {
       console.log(err);
     }
   };
 
-  const updateTask = async (id, newName) => {
-    try {
-      
-      await apiClient.put(`/task/${id}`, { name: newName });
-      await fetchTodo(); // Refresh task list
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const removeTask = async (id) => {
-    try {
-      // Assuming you have an endpoint to remove tasks
-      await apiClient.delete(`/task/${id}`);
-      await fetchTodo(); // Refresh task list
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // const toggleTaskCompletion = async (id) => {
-  //   try {
-  //     // Assuming you have an endpoint to toggle task completion
-  //     await apiClient.patch(`/task/${id}/toggle`);
-  //     await fetchTodo(); // Refresh task list
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
 
   return {
     fetchTodo,
     taskList,
-    addTask,
-    updateTask,
-    removeTask,
-    // toggleTaskCompletion
   };
 }
